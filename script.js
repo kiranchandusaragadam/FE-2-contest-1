3fd94396dd1248706d40c8a66f03c5c4c2168c4d
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2024-01-13 18:43:19 +0530

    assignment done

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((e) => {
+    if (e.profession === "developer") {
+      console.log(e);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((e) => {
+    if (e.profession === "developer") {
+      console.log(e);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmp = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmp);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((e) => {
+    return e.profession != "admin";
+  });
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 5, name: "Doe", age: "28", profession: "SDE" },
+    { id: 6, name: "Kiran", age: "25", profession: "developer" },
+    { id: 7, name: "Mahesh", age: "24", profession: "HR" },
+  ];
+
+  let wholeArr = arr.concat(arr2);
+  console.log(wholeArr);
 }
